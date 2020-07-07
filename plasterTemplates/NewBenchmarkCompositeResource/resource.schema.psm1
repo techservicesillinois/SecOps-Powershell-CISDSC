7d2233a0756a192a81cee359290c65e622c53146
MKletz <michael.kletz.27@gmail.com>
2020-07-07 11:24:25 -0500

    removed node block because thats not a thing here

--- plasterTemplates/NewBenchmarkCompositeResource/resource.schema.psm1
+++ plasterTemplates/NewBenchmarkCompositeResource/resource.schema.psm1
@@ -2,35 +2,32 @@ Configuration CIS_<%=$PLASTER_PARAM_Level%>_<%=$PLASTER_PARAM_OS%>_Release_<%=$P
 {
     param
     (
-        [string]
-        $Node = 'localhost',
-
         [string[]]
-        $ExcludeList
+        $ExcludeList,
+
+        [String]
+        $LocalAdminNewName,
+
+        [string]
+        $LocalGuestNewName
     )
 
     Import-DSCResource -ModuleName 'PSDesiredStateConfiguration'
     Import-DSCResource -ModuleName 'AuditPolicyDSC' -ModuleVersion '1.4.0.0'
     Import-DSCResource -ModuleName 'SecurityPolicyDSC' -ModuleVersion '2.10.0.0'
 
-    Node $Node
-    {
-        #region 1 Category name
+    #region 1 Category name
 
-        #region 1.1 Section name
-        if($ExcludeList -notcontains '1.1.1'){
-        }
-        #endregion
+    #region 1.1 Section name
+    if($ExcludeList -notcontains '1.1.1'){
+    }
+    #endregion
 
-        #endregion
+    #endregion
 
-        #region 2 Category name
+    #region 2 Category name
 
-        #region 2.1 Section name
-        if($ExcludeList -notcontains '2.1.1'){
-        }
-        #endregion
-
-        #endregion
+    #region 2.1 Section name
+    if($ExcludeList -notcontains '2.1.1'){
     }
 }
