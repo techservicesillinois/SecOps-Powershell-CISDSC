9e3d6656af85ae324831ca0bc247129b235a1046
MKletz <michael.kletz.27@gmail.com>
2020-07-27 09:22:28 -0500

    pester tweaks

--- test/CISDSC.Tests.ps1
+++ test/CISDSC.Tests.ps1
@@ -1,9 +1,19 @@
-[String]$ModuleRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src\CISDSC'
-Import-Module -Name $ModuleRoot
+[String]$SourceRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src'
+[String]$ModuleRoot = Join-Path -Path $SourceRoot -ChildPath 'CISDSC'
+($ENV:PSModulePath.split(';')) + $SourceRoot -join ';'
+Import-Module -Name 'CISCDSC'
 
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         $ManifestPath = Join-Path -Path "$(Split-Path -Path $PSScriptRoot -Parent)" -ChildPath 'src\CISDSC\CISDSC.psd1'
         Test-ModuleManifest -Path $ManifestPath | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Exports all of its DSCResources' {
+        $CompareSplat = @{
+            ReferenceObject = (Get-Module -Name 'CISDSC' -ListAvailable).ExportedDscResources
+            DifferenceObject = (Get-ChildItem -Path "$($ModuleRoot)\dscresources" -Directory).Name
+        }
+        Compare-Object @CompareSplat | Should -Be $null
     }
 }
--- test/CISDSCResourceGeneration.Tests.ps1
+++ test/CISDSCResourceGeneration.Tests.ps1
@@ -1,5 +1,7 @@
-[String]$ModuleRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src\CISDSCResourceGeneration'
-Import-Module -Name $ModuleRoot
+[String]$SourceRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src'
+[String]$ModuleRoot = Join-Path -Path $SourceRoot -ChildPath 'CISDSCResourceGeneration'
+($ENV:PSModulePath.split(';')) + $SourceRoot -join ';'
+Import-Module -Name 'CISDSCResourceGeneration'
 
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
