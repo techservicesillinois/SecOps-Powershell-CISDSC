a2e848048f5cfb17459c1024441b3b21d7872c3e
MKletz <michael.kletz.27@gmail.com>
2020-08-14 13:11:58 -0500

    added exclusions

--- src/CISDSC/Examples/CIS_Microsoft_Windows_10_Enterprise_Release_1909.ps1
+++ src/CISDSC/Examples/CIS_Microsoft_Windows_10_Enterprise_Release_1909.ps1
@@ -6,6 +6,8 @@ Configuration Win10_1909
     {
         CIS_Microsoft_Windows_10_Enterprise_Release_1909 'CIS Benchmarks'
         {
+            #These exclusions are services that are not in default installs of Windows. Remove the exlusions if they are applicable to your envrionment.
+            'ExcludeList' = @('5.6','5.7','5.10','5.11','5.14','5.28','5.32','5.40')
             '2315AccountsRenameadministratoraccount' = 'CISAdmin'
             '2316AccountsRenameguestaccount' = 'CISGuest'
             '2375LegalNoticeText' = 'Legal says only we can use these machines.'
