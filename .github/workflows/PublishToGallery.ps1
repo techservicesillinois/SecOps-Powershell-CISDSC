0b8d56ce8e013ce390ab1fc3263485e4d81e371d
MKletz <michael.kletz.27@gmail.com>
2020-08-18 15:18:43 -0500

    added publish action

---
+++ .github/workflows/PublishToGallery.ps1
@@ -0,0 +1,9 @@
+try{
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Name 'PSDepend' -Force -SkipPublisherCheck
+    Invoke-PSDepend -Force
+    Publish-Module -Path ".\src\CISDSC" -Repository PSGallery -NuGetApiKey $ENV:NuGetApiKey -Force
+}
+catch{
+    throw $_
+}
