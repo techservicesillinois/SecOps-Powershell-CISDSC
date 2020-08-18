58519d31d78b6c8dfdbad556a21f9ff58cd75055
MKletz <michael.kletz.27@gmail.com>
2020-08-18 14:56:39 -0500

    more complete example

--- src/CISDSC/Examples/CIS_Microsoft_Windows_10_Enterprise_Release_1909.ps1
+++ src/CISDSC/Examples/CIS_Microsoft_Windows_10_Enterprise_Release_1909.ps1
@@ -1,6 +1,6 @@
 Configuration Win10_1909_L1
 {
-    Import-DSCResource -ModuleName 'CISDSC'
+    Import-DSCResource -ModuleName 'CISDSC' -Name 'CIS_Microsoft_Windows_10_Enterprise_Release_1909'
 
     node 'localhost'
     {
@@ -10,12 +10,18 @@ Configuration Win10_1909_L1
             'ExcludeList' = @('5.6','5.7','5.10','5.11','5.14','5.28','5.32','5.40')
             '2315AccountsRenameadministratoraccount' = 'CISAdmin'
             '2316AccountsRenameguestaccount' = 'CISGuest'
-            '2375LegalNoticeText' = 'Legal says only we can use these machines.'
             '2376LegalNoticeCaption' = 'Legal Notice'
+            '2375LegalNoticeText' = @"
+This is a super secure device that we don't want bad people using.
+I'm even making sure to put this as a literal string so that I can cleanly
+use multiple lines to tell you how super secure it is.
+"@
         }
     }
 }
 
 Win10_1909_L1
 #Uncomment the following line to also apply the configuration on the localmachine
-#Start-DscConfiguration -Path '.\Win10_1909_L1' -Verbose -Wait
+#Start-DscConfiguration -Path '.\Win10_1909_L1' -Verbose -Wait -Force
+#This version will capture errors and verbose to a log file
+#Start-DscConfiguration -Path '.\Win10_1909_L1' -Verbose -Wait -Force 4>&1 2>&1 > 'c:\DSC.log'
