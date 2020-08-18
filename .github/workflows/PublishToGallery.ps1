24d64c2d48e83208ee789860b9da0e4bf130c63e
MKletz <michael.kletz.27@gmail.com>
2020-08-18 15:21:19 -0500

    quotes

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,8 +1,8 @@
 try{
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
     Install-Module -Name 'PSDepend' -Force -SkipPublisherCheck
     Invoke-PSDepend -Force
-    Publish-Module -Path ".\src\CISDSC" -Repository PSGallery -NuGetApiKey $ENV:NuGetApiKey -Force
+    Publish-Module -Path '.\src\CISDSC' -Repository 'PSGallery' -NuGetApiKey $ENV:NuGetApiKey -Force
 }
 catch{
     throw $_
