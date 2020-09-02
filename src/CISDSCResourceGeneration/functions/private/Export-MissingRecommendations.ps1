4b54322141b422c7ae696f7c900a4806b95cdbb0
MKletz <michael.kletz.27@gmail.com>
2020-09-02 11:12:49 -0500

    Replaced Find-MissingRecommendations with Export-MissingRecommendations

---
+++ src/CISDSCResourceGeneration/functions/private/Export-MissingRecommendations.ps1
@@ -0,0 +1,36 @@
+<#
+.Synopsis
+   Exports a list of imported recommendations that did not have a setting identified in the provided GPOs.
+.DESCRIPTION
+   Exports a list of imported recommendations that did not have a setting identified in the provided GPOs.
+.PARAMETER OutputPath
+    Output directory for the files generated.
+.EXAMPLE
+#>
+function Export-MissingRecommendations {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$OutputPath
+    )
+
+    begin {
+
+    }
+
+    process {
+        [System.Collections.ArrayList]$MissingRecommendations = @()
+        #The user section is ignored for this check because user based settings are not possible via DSC and will always be blank.
+        #18.2.1 (L1) Ensure LAPS AdmPwd GPO Extension / CSE is installed is explictly ignored because this is a software installation that won't be supported at this time.
+        $MissingRecommendations = ($script:BenchmarkRecommendations).Values | Where-Object -FilterScript {!$_.ResourceParameters -and $_.TopLevelSection -ne $script:UserSection -and $_.RecommendationNum -ne '18.2.1'}
+
+        if($MissingRecommendations){
+            [string]$RecommendationErrorsPath = Join-Path -Path $OutputPath -ChildPath 'MissingRecommendation.csv'
+            $MissingRecommendations | Export-Csv -Path $RecommendationErrorsPath -NoTypeInformation -Force
+        }
+    }
+
+    end {
+
+    }
+}
