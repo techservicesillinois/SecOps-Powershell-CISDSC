7b840d36c3320f769973b88b099706425778cfae
MKletz <michael.kletz.27@gmail.com>
2020-09-17 11:30:30 -0500

    made installs explicit (#104)
    * made installs explicit
    
    * made install dynamic

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,7 +1,10 @@
 try{
     Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
-    Install-Module -Name 'PSDepend' -Force -SkipPublisherCheck
-    Invoke-PSDepend -Force
+
+    (Import-PowerShellDataFile -Path '.\src\CISDSC\CISDSC.psd1')['RequiredModules'] | ForEach-Object -Process {
+        Install-Module -Name $_['ModuleName'] -RequiredVersion $_['ModuleVersion'] -Force -SkipPublisherCheck
+    }
+
     Publish-Module -Path '.\src\CISDSC' -Repository 'PSGallery' -NuGetApiKey $ENV:NuGetApiKey -Force
 }
 catch{
