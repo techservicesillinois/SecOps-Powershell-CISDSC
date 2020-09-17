5056da4d399c2cf7e59b9f19ec4182decba4bbb7
MKletz <michael.kletz.27@gmail.com>
2020-09-17 09:51:31 -0500

    moved dependency install

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,7 +1,5 @@
 try{
     Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
-    Install-Module -Name 'PSDepend' -Force -SkipPublisherCheck
-    Invoke-PSDepend -Force
     Publish-Module -Path '.\src\CISDSC' -Repository 'PSGallery' -NuGetApiKey $ENV:NuGetApiKey -Force
 }
 catch{
