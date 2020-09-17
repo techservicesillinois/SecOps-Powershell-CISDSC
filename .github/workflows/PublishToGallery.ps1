c1ac0878d44af3434284474a363fd1c4c3906bb4
MKletz <michael.kletz.27@gmail.com>
2020-09-17 09:58:47 -0500

    made installs explicit

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,7 +1,7 @@
 try{
     Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
-    Install-Module -Name 'PSDepend' -Force -SkipPublisherCheck
-    Invoke-PSDepend -Force
+    Install-Module -Name 'AuditPolicyDSC' -RequiredVersion '1.4.0.0' -Force -SkipPublisherCheck
+    Install-Module -Name 'SecurityPolicyDSC' -RequiredVersion '2.10.0.0' -Force -SkipPublisherCheck
     Publish-Module -Path '.\src\CISDSC' -Repository 'PSGallery' -NuGetApiKey $ENV:NuGetApiKey -Force
 }
 catch{
