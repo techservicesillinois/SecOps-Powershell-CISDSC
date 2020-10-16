25c433166d70c15d3eb39d5681d64b05f782075f
MKletz <michael.kletz.27@gmail.com>
2020-10-16 15:44:33 -0500

    Server2019 1809 resource (#145)
    * CSVs
    
    * CSVs
    
    * #7
    
    * changelog
    
    * casing
    
    * doc correction
    
    * doc corrections 2

---
+++ src/CISDSC/Examples/CIS_Microsoft_Windows_Server_2019_Member_Server_Release_1809.ps1
@@ -0,0 +1,30 @@
+#Requires -module CISDSC
+
+<#
+    .DESCRIPTION
+    Applies CIS Level one benchmarks for Microsoft_Windows_Server_2019_Member_Server build 1809 the no exclusions.
+    Exclusion documentation can be found in the docs folder of this module.
+#>
+
+Configuration Microsoft_Windows_Server_2019_Member_Server_1809_CIS_L1
+{
+    Import-DSCResource -ModuleName 'CISDSC' -Name 'CIS_Microsoft_Windows_Server_2019_Member_Server_Release_1809'
+
+    node 'localhost'
+    {
+        CIS_Microsoft_Windows_Server_2019_Member_Server_Release_1809 'CIS Benchmarks'
+        {
+            '2315AccountsRenameadministratoraccount' = 'CISAdmin'
+            '2316AccountsRenameguestaccount' = 'CISGuest'
+            '2375LegalNoticeCaption' = 'Legal Notice'
+            '2374LegalNoticeText' = @"
+This is a super secure device that we don't want bad people using.
+I'm even making sure to put this as a literal string so that I can cleanly
+use multiple lines to tell you how super secure it is.
+"@
+        }
+    }
+}
+
+Microsoft_Windows_Server_2019_Member_Server_1809_CIS_L1
+Start-DscConfiguration -Path '.\Microsoft_Windows_Server_2019_Member_Server_1809_CIS_L1' -Verbose -Wait
---
+++ src/CISDSC/Examples/CIS_Microsoft_Windows_Server_2019_Member_Server_Release_1809_With_LAPS.ps1
@@ -0,0 +1,39 @@
+#Requires -module CISDSC
+
+<#
+    .DESCRIPTION
+    Applies CIS Level one benchmarks for Microsoft_Windows_Server_2019_Member_Server build 1809 the no exclusions.
+    Exclusion documentation can be found in the docs folder of this module.
+    This will also install LAPS (Local Administrator Password Solution) from the internet via download.microsoft.com unless the URL is changed to a network accesible path for your envrionment.
+#>
+
+Configuration Microsoft_Windows_Server_2019_Member_Server_1809_CIS_L1_with_LAPS
+{
+    Import-DSCResource -ModuleName 'PSDesiredStateConfiguration'
+    Import-DSCResource -ModuleName 'CISDSC' -Name 'CIS_Microsoft_Windows_Server_2019_Member_Server_Release_1809'
+
+    node 'localhost'
+    {
+        Package 'InstallLAPS' {
+            Name  = 'Local Administrator Password Solution'
+            Path = 'https://download.microsoft.com/download/C/7/A/C7AAD914-A8A6-4904-88A1-29E657445D03/LAPS.x64.msi'
+            ProductId = 'EA8CB806-C109-4700-96B4-F1F268E5036C'
+        }
+
+        CIS_Microsoft_Windows_Server_2019_Member_Server_Release_1809 'CIS Benchmarks'
+        {
+            '2315AccountsRenameadministratoraccount' = 'CISAdmin'
+            '2316AccountsRenameguestaccount' = 'CISGuest'
+            '2375LegalNoticeCaption' = 'Legal Notice'
+            '2374LegalNoticeText' = @"
+This is a super secure device that we don't want bad people using.
+I'm even making sure to put this as a literal string so that I can cleanly
+use multiple lines to tell you how super secure it is.
+"@
+            'DependsOn' = '[Package]InstallLAPS'
+        }
+    }
+}
+
+Microsoft_Windows_Server_2019_Member_Server_1809_CIS_L1_with_LAPS
+Start-DscConfiguration -Path '.\Microsoft_Windows_Server_2019_Member_Server_1809_CIS_L1_with_LAPS' -Verbose -Wait
