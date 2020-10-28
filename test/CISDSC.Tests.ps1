ed29dbd73464af72885ff9a73f5b044930cd36dc
MKletz <michael.kletz.27@gmail.com>
2020-10-28 10:57:59 -0500

    Slow pester action fix attempt (#158)
    * attempt one
    
    * pwsh test
    
    * comment
    
    * comment
    
    * warning
    
    * added additional manifest test
    
    * dependency change
    
    * removed psdepend
    
    * line

--- test/CISDSC.Tests.ps1
+++ test/CISDSC.Tests.ps1
@@ -2,13 +2,20 @@
 Import-Module -Name $ModuleRoot
 
 Describe 'Module Manifest Tests' {
-    It 'Passes Test-ModuleManifest' {
+    It 'Module: Passes Test-ModuleManifest' {
         $ManifestPath = Join-Path -Path "$(Split-Path -Path $PSScriptRoot -Parent)" -ChildPath 'src\CISDSC\CISDSC.psd1'
         Test-ModuleManifest -Path $ManifestPath | Should -Not -BeNullOrEmpty
     }
+
+    It 'DSCResources: Passes Test-ModuleManifest' {
+        $ResourcePath = Join-Path -Path "$(Split-Path -Path $PSScriptRoot -Parent)" -ChildPath 'src\CISDSC\dscresources'
+        Get-ChildItem -Path $ResourcePath -Filter "*.psd1" | ForEach-Object -Process {
+            Test-ModuleManifest -Path $_.FullName | Should -Not -BeNullOrEmpty
+        }
+    }
 }
 
-Describe 'Class: CISService' {    
+Describe 'Class: CISService' {
     InModuleScope -ModuleName 'CISDSC' {
         It 'Fails a running service' {
             . "$($PSScriptRoot)\New-PSClassInstance.ps1"
---
+++ tools/install_dependencies.ps1
@@ -0,0 +1,3 @@
+Install-Module -Name ('ImportExcel','GPRegistryPolicyParser','Plaster','pester') -Scope AllUsers -Force
+Install-Module -Name 'AuditPolicyDSC' -RequiredVersion '1.4.0.0' -Scope AllUsers -Force
+Install-Module -Name 'SecurityPolicyDSC' -RequiredVersion '2.10.0.0' -Scope AllUsers -Force
