6e0f74d85983658e5571b6fbad7296c500d71622
MKletz <michael.kletz.27@gmail.com>
2021-02-25 13:11:59 -0600

    warning to debug

--- src/CISDSCResourceGeneration/functions/private/Import-GptTmpl.ps1
+++ src/CISDSCResourceGeneration/functions/private/Import-GptTmpl.ps1
@@ -31,9 +31,9 @@ function Import-GptTmpl {
                 foreach($subKey in $ini[$key].Keys){
                     switch($key){
                         #Encoding statement for the file that we don't care about.
-                        'Unicode' {Write-Verbose -Message 'Skipping Unicode section'}
+                        'Unicode' {Write-Debug -Message 'Skipping Unicode section'}
                         #Version statement for the file that we also don't care about.
-                        'Version' {Write-Verbose -Message 'Skipping Version section'}
+                        'Version' {Write-Debug -Message 'Skipping Version section'}
                         'Registry Values' {ConvertFrom-RegistryValueRawGPO -Key $subkey -ValueData $ini[$key][$subKey]}
                         'Privilege Rights' {ConvertFrom-PrivilegeRightRawGPO -Policy $subkey -Identity $ini[$key][$subKey]}
                         'System Access' {ConvertFrom-SystemAccessRawGPO -Key $subkey -SecurityData $ini[$key][$subKey]}
