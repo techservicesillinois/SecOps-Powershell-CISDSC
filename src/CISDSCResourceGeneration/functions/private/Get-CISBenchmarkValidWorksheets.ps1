37103e675d26bad3db4a830f6aae5c2208624d64
MKletz <michael.kletz.27@gmail.com>
2021-04-12 12:49:32 -0500

    filters

--- src/CISDSCResourceGeneration/functions/private/Get-CISBenchmarkValidWorksheets.ps1
+++ src/CISDSCResourceGeneration/functions/private/Get-CISBenchmarkValidWorksheets.ps1
@@ -1,14 +1,14 @@
 <#
 .Synopsis
-   Takes a CIS Excel benchmark and operating system name to determine what worksheets inside the benchmark are relevant.
-   Primairly filters out the License and the opposite worksheets for member server and domain controller.
+    Takes a CIS Excel benchmark and operating system name to determine what worksheets inside the benchmark are relevant.
+    Primairly filters out the License and the opposite worksheets for member server and domain controller.
 .DESCRIPTION
-   Takes a CIS Excel benchmark and operating system name to determine what worksheets inside the benchmark are relevant.
-   Primairly filters out the License and the opposite worksheets for member server and domain controller.
+    Takes a CIS Excel benchmark and operating system name to determine what worksheets inside the benchmark are relevant.
+    Primairly filters out the License and the opposite worksheets for member server and domain controller.
 .PARAMETER Path
     Path to the excel document from CIS for the benchmark documentation.
-.PARAMETER OS
-    The operating system the benchmark is written for. This will seperate member server and domain controller worksheets.
+.PARAMETER System
+    The operating system or software the benchmark is written for. This will determine which worksheets are valid to import.
 .EXAMPLE
     Get-CISBenchmarkValidWorksheet -Path '.\CIS_Microsoft_Windows_10_Enterprise_Release_1909_Benchmark_v1.8.1.xlsx' -OS 'Microsoft Windows 10 Enterprise'
 #>
@@ -25,21 +25,20 @@ function Get-CISBenchmarkValidWorksheets {
     )
 
     begin {
-
     }
 
     process {
         switch($System){
             {$_ -like '*Member Server'}{
-                $FilterScript = {$_.Name -ne 'License' -and $_.Name -notlike "*Domain Controller"}
+                $FilterScript = {$_.Name -like "*Member Server"}
             }
 
             {$_ -like '*Domain Controller'}{
-                $FilterScript = {$_.Name -ne 'License' -and $_.Name -notlike "*Member Server"}
+                $FilterScript = {$_.Name -like"*Member Server"}
             }
 
             Default{
-                $FilterScript = {$_.Name -ne 'License'}
+                $FilterScript = {$_.Name -notin ('License','Overview - Glossary') -and $_.Name -notlike "MITRE*" }
             }
         }
 
@@ -47,6 +46,5 @@ function Get-CISBenchmarkValidWorksheets {
     }
 
     end {
-
     }
 }
