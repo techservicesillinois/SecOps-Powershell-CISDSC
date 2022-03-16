6b4b4816a6acc9331b3ed46f021e0e3f1bf73d37
baristaTam <git@tamarabee.com>
2022-03-16 15:49:55 -0500

    character limits

--- src/CISDSCResourceGeneration/CISDSCResourceGeneration.psm1
+++ src/CISDSCResourceGeneration/CISDSCResourceGeneration.psm1
@@ -110,9 +110,11 @@ Class Recommendation{
         $This.SectionNum = $ExcelRow.'section #'
         $This.RecommendationNum = $ExcelRow.'recommendation #'
         #Quotes are normalized for consistentcy in checks.
-        $This.Title = $ExcelRow.Title.Replace('"',"'")
+        $This.Title = ($ExcelRow.Title.Replace('"',"'"))
         #Most special characters are filtered out of the title for the DSC resource. This prevents various encoding issues. Recommendation number is added to the start to prevent duplicate titles between recommendations which does occur.
         $This.DSCTitle = "$($This.RecommendationNum) - $($This.Title)" -replace "[^a-zA-Z0-9-() .]",""
+        #DSCTitle needs to be truncated to 165 chars to avoid limitations in Azure
+        $This.DSCTitle = ($This.DSCTitle).Substring(0, [System.Math]::Min(165, $This.DSCTitle.length))
         $This.Description = $ExcelRow.Description
         $This.RemediationProcedure = $ExcelRow.'remediation procedure'
         $This.AuditProcedure = $ExcelRow.'audit procedure'
