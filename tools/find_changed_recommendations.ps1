d3607857b8c566332cf8d120b8ce14d83faf1c3e
Mike Kletz <michael.kletz.27@gmail.com>
2022-06-12 23:28:56 -0500

    updated tool script

--- tools/find_changed_recommendations.ps1
+++ tools/find_changed_recommendations.ps1
@@ -9,6 +9,10 @@
     Path to the new version of the Excel file.
 .PARAMETER System
     Type of benchmark. Ex: Workstation, Member Server, Domain Controller
+.PARAMETER CSVPrefix
+    Starting text of the CSVs IE: "Server2019_20H2", "Win10_1809"
+.PARAMETER ForkCSVs
+    If provided it will make new CSVs with the changed IDs updated
 #>
 
 param (
@@ -22,7 +26,9 @@ param (
         'Member Server',
         'Domain Controller'
     )]
-    [string]$System
+    [string]$System,
+    [string]$CSVPrefix,
+    [switch]$ForkCSVs
 )
 
 Import-Module '..\src\CISDSCResourceGeneration\CISDSCResourceGeneration.psd1' -Force
@@ -55,6 +61,28 @@ $Properties = @(
     }
 )
 
-Compare-Object -ReferenceObject $PreviousBenchmark -DifferenceObject $NewBenchmark -Property ('title','Recommendation #') |
+$Changes = Compare-Object -ReferenceObject $PreviousBenchmark -DifferenceObject $NewBenchmark -Property ('title','Recommendation #') |
     Group-Object -Property 'title' |
-    Select-Object -Property $Properties
+    Select-Object -Property $Properties |
+    Where-Object -FilterScript {$_.OldNumber -and $_.NewNumber}
+
+if($ForkCSVs.IsPresent){
+    Get-ChildItem -Path "..\csvs" -Recurse -Filter "$($CSVPrefix)*.csv" | ForEach-Object -Process {
+        $NewName = "$($_.Name)_UPDATED.csv"
+        Copy-Item -Path $_.FullName -Destination $NewName
+        $Content = Get-Content -Path $NewName
+        $Changes | Where-Object -FilterScript { ($_.NewNumber | Measure-Object).count -eq 1 } | ForEach-Object -Process {
+            $Content = $Content.replace($_.OldNumber,$_.NewNumber)
+        }
+        Set-Content -Path $NewName -Value $Content
+    }
+
+    $ManualChecks = $Changes | Where-Object -FilterScript { ($_.NewNumber | Measure-Object).count -gt 1 }
+    if($ManualChecks){
+        Write-Output -InputObject 'The following need manual intervention because they had multiple matches'
+        $ManualChecks
+    }
+}
+else{
+    $Changes
+}
