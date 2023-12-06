8da80c3308b8d0385fa9d94a9041ee975eae127d
Mark Wenneborg <wennebo1@illinois.edu>
2023-12-06 15:35:06 -0600

    added wip_recommendations

--- tools/generate_resources_changes.ps1
+++ tools/generate_resources_changes.ps1
@@ -1,14 +1,14 @@
-Import-Module '..\src\CISDSCResourceGeneration\CISDSCResourceGeneration.psd1' -Force
+Import-Module '.\src\CISDSCResourceGeneration\CISDSCResourceGeneration.psd1' -Force
 Remove-Item -Path '.\output' -Recurse -Force -ErrorAction 'SilentlyContinue'
 
 $Splat = @{
-    BenchmarkPath    = "C:\Users\asvalent\Downloads\CIS_Microsoft_Windows_11_Enterprise_Benchmark_v2.0.0.xlsx"
+    BenchmarkPath    = "C:\Users\wennebo1\Downloads\CIS_Microsoft_Windows_11_Enterprise_Benchmark_v2.0.0.xlsx"
     BenchmarkVersion = '2.0.0'
-    GPOPath          = 'C:\Users\asvalent\Downloads\Windows11v2.0.0'
-    OutputPath       = 'C:\Users\asvalent\repo\SecOps-Powershell-CISDSC\src\CISDSC\dscresources'
-    StaticCorrectionsPath    = 'C:\Users\asvalent\repo\SecOps-Powershell-CISDSC\csvs\static_corrections\Win11_23H2_corrections.csv'
-    ParameterValidationsPath = 'C:\Users\asvalent\repo\SecOps-Powershell-CISDSC\csvs\parameter_validations\Win11_23H2_validations.csv'
-    ParameterOverridesPath   = 'C:\Users\asvalent\repo\SecOps-Powershell-CISDSC\csvs\parameter_overrides\Win11_23H2_overrides.csv'
+    GPOPath          = 'C:\Users\wennebo1\Downloads\Windows11v2.0.0'
+    OutputPath       = 'C:\Users\wennebo1\Git\techservicesillinois\SecOps-Powershell-CISDSC\src\CISDSC\dscresources'
+    StaticCorrectionsPath    = 'C:\Users\wennebo1\Git\techservicesillinois\SecOps-Powershell-CISDSC\csvs\static_corrections\Win11_23H2_corrections.csv'
+    ParameterValidationsPath = 'C:\Users\wennebo1\Git\techservicesillinois\SecOps-Powershell-CISDSC\csvs\parameter_validations\Win11_23H2_validations.csv'
+    ParameterOverridesPath   = 'C:\Users\wennebo1\Git\techservicesillinois\SecOps-Powershell-CISDSC\csvs\parameter_overrides\Win11_23H2_overrides.csv'
 
     # uncomment which is applicable
 
