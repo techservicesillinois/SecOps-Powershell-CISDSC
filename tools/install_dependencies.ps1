8fa2e4e5c318046da328ad55fb241a1262ad87d9
Edward Delaporte <delaport@illinois.edu>
2024-01-16 15:58:49 -0600

    WIP - And back to Windows

--- tools/install_dependencies.ps1
+++ tools/install_dependencies.ps1
@@ -1,4 +1,5 @@
-Install-Module -Name ('ImportExcel', 'Plaster', 'pester') -Scope CurrentUser -Force
-Install-Module -Name 'GPRegistryPolicyParser' -AllowPrerelease -Scope CurrentUser -Force
-Install-Module -Name 'AuditPolicyDSC' -RequiredVersion '1.4.0.0' -Scope CurrentUser -Force
-Install-Module -Name 'SecurityPolicyDSC' -RequiredVersion '2.10.0.0' -Scope CurrentUser -Force
+# TODO: Add support for -Scope CurrentUser to support CI/CD
+Install-Module -Name ('ImportExcel', 'Plaster', 'pester') -Scope AllUsers -Force
+Install-Module -Name 'GPRegistryPolicyParser' -RequiredVersion '1.0.0' -Scope AllUsers -Force
+Install-Module -Name 'AuditPolicyDSC' -RequiredVersion '1.4.0.0' -Scope AllUsers -Force
+Install-Module -Name 'SecurityPolicyDSC' -RequiredVersion '2.10.0.0' -Scope AllUsers -Force
